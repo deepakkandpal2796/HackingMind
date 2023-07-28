cdd24b042f3ba532bf299f1b87fc9c6359d1d770
deepakkandpal2796 <deepakkandpal2796@gmail.com>
2023-07-28 21:24:56 +0530

    Thrusday

--- JS_Jonas/MyCode/06-Modal/starter/script.js
+++ JS_Jonas/MyCode/06-Modal/starter/script.js
@@ -1,1 +1,57 @@
 'use strict';
+
+//Selecting elements 
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+// Functions 
+const showModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const removeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+//Events 
+//We want to add event to all the modal buttons 
+//Show modal button event 
+for(let i=0; i <btnOpenModal.length; i++){
+    btnOpenModal[i].addEventListener('click', showModal)
+}
+
+//Close modal button event 
+btnCloseModal.addEventListener('click',removeModal)
+
+//Overlay event (click on the overlay to remove modal)
+overlay.addEventListener('click', removeModal)
+
+//Escape key event 
+//! 1st the key presseed should be escape and we also have to check that the modal is visible that means it dosent contain the hidden class so pressing escape hide the modal.
+
+document.addEventListener('keydown', function(e){
+    // console.log(e,e.key);
+    // if(e.key === 'Escape'){
+    //     if(!modal.classList.contains('hidden')){
+    //         removeModal();
+    //     }
+    // }
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        removeModal();
+    }
+})
+
+//!Findings
+//Dont call the functin in the addEventListners method it should we called when the element get clicked.
+
+//Make all the things seprate for writting clean code
+
+//To make keyboard event work we add the event listner to the document , and use keyDown event 
+
+// If we pass the attribute in that function we will get the keyboard event in which key property will give us the name of the key 
+
+//Check the condition is very necessary like here we have check that if the modal is already visible or not to. and to check these things we use classList.contains() method. 
--- JS_Jonas/MyCode/07-Pig-Game/starter/script.js
+++ JS_Jonas/MyCode/07-Pig-Game/starter/script.js
@@ -1,1 +1,69 @@
 'use strict';
+//Selecting Elements 
+//players 
+const player0El = document.querySelector('player--0');
+const player1El = document.querySelector('player--1');
+//score
+const score0El = document.getElementById('score--0');
+const score1El = document.querySelector('#score--1');
+//current score
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+//buttons
+const btnNew = document.getElementsByClassName('btn--new')[0];
+const btnRoll = document.getElementsByClassName('btn--roll')[0];
+const btnHold = document.querySelector('.btn--hold');
+//dice
+const diceEl = document.getElementsByClassName('dice')[0];
+
+
+
+//Starting conditions 
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+let currentScore = 0;
+let activePlayer = 0;
+
+//Function 
+const switchPlayer = () => {
+    //*remove active player class 
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    //*make current score zero
+    document.getElementById(`current--${activePlayer}`).innerHTML = 0;
+    //*change the active player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    //*add the active player class to other player 
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--active')
+}
+
+//Events 
+btnRoll.addEventListener('click', function (){
+    //* 1. Generating a random dice roll 
+    const dice = Math.trunc(Math.random()*6)+1;
+    console.log(dice);
+
+    //* 2. Display Dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //* 3. When it's not 1 add to current score
+    if(dice != 1){
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).innerHTML = currentScore;
+    }
+    else{
+        //* 4. when its 1 then make the current score 0 and switch the active player 
+        currentScore = 0;
+        // document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        // document.getElementById(`current--${activePlayer}`).innerHTML = 0;
+        // activePlayer = activePlayer === 0 ? 1 : 0;
+        // document.querySelector(`.player--${activePlayer}`).classList.add('player--active')
+        switchPlayer();
+    }
+});
+
+//hold button 
+btnHold.addEventListener('click', function() {
+    document.getElementById(`score--${activePlayer}`);
+})
