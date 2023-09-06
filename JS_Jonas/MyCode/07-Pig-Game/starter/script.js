877665d541388637754143888e538a828c4b207b
deepakkandpal2796 <deepakkandpal2796@gmail.com>
2023-09-06 21:55:05 +0530

    restarting

--- JS_Jonas/MyCode/07-Pig-Game/starter/script.js
+++ JS_Jonas/MyCode/07-Pig-Game/starter/script.js
@@ -1,71 +1,11 @@
 'use strict';
-//Selecting Elements 
-//players 
-const player0El = document.querySelector('player--0');
-const player1El = document.querySelector('player--1');
-//score
-const score0El = document.getElementById('score--0');
+
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
-//current score
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
-//buttons
-const btnNew = document.getElementsByClassName('btn--new')[0];
-const btnRoll = document.getElementsByClassName('btn--roll')[0];
-const btnHold = document.querySelector('.btn--hold');
-//dice
-const diceEl = document.getElementsByClassName('dice')[0];
+const diceEl = document.querySelector('.dice');
 
-
-
-//Starting conditions 
-score0El.textContent = 0;
-score1El.textContent = 0;
+// Starting conditions
+const score0 = score0El.textContent = 0;
+const score1 = score1El.textContent = 0;
 diceEl.classList.add('hidden');
-let currentScore = 0;
-let activePlayer = 0;
-
-//Function 
-const switchPlayer = () => {
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-}
-
-//Events 
-btnRoll.addEventListener('click', function (){
-    //* 1. Generating a random dice roll 
-    const dice = Math.trunc(Math.random()*6)+1;
-    console.log(dice);
-
-    //* 2. Display Dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
-
-    //* 3. When it's not 1 add to current score
-    if(dice != 1){
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).innerHTML = currentScore;
-    }
-    else{
-        //* 4. when its 1 then make the current score 0 and switch the active player 
-        currentScore = 0;
-        // //*remove active player class 
-        // document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
-        // //*make current score zero
-        // document.getElementById(`current--${activePlayer}`).innerHTML = 0;
-        // //*change the active player
-        // activePlayer = activePlayer === 0 ? 1 : 0;
-        // //*add the active player class to other player 
-        // document.querySelector(`.player--${activePlayer}`).classList.add('player--active')
-        // switchPlayer();
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
-    }
-});
-
-//hold button 
-btnHold.addEventListener('click', function() {
-    document.getElementById(`score--${activePlayer}`);
-})
--- JS_Jonas/MyCode/09-Data-Structures-Operators/starter/script.js
+++ JS_Jonas/MyCode/09-Data-Structures-Operators/starter/script.js
@@ -11,7 +11,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
+  order: function(startIndex, mainIndex){
+    return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +29,97 @@ const restaurant = {
     },
   },
 };
+
+// Destructuring is used to break the big datastructures into the smaller ones 
+
+//*=========================Destructuring of arrays=========================
+
+//?The Noob way
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+
+//?Destructuring
+const [x, y, z] = arr;
+console.log(x, y, z); // 2 3 4
+console.log(arr); // [ 2, 3, 4 ]
+
+//? Take only two elements 
+const [first, second] = restaurant.categories;
+console.log(first, second); //Italian Pizzeria
+
+//?Skip any element 
+let [starter, ,main] = restaurant.starterMenu;
+console.log(restaurant.starterMenu) //[ 'Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad' ]  
+console.log(starter, main); // Focaccia Garlic Bread
+
+//swaping variables 
+[starter, main] = [main, starter];
+console.log(starter, main); // Garlic Bread Focaccia
+
+//? Recieve 2 retrun value from a function
+const [start, secondary] = restaurant.order(0, 2); //! remember this function return the array 
+console.log(start, secondary); // Focaccia Risotto
+
+//?Nested array destructuring  
+const nested = [1, 2, [2, 4]];
+const [i, ,j] = nested;
+console.log(nested); // [ 1, 2, [ 2, 4 ] ]
+console.log(i, j); // 1 [ 2, 4 ]
+
+//?Nested destructuring (destructuring inside destructuring)
+const [x1, , [y1, z1]] = nested;
+console.log(x1, y1, z1); // 1 2 4
+
+//? Default values 
+const [p, q, r] = [8, 9]
+console.log(p, q, r); // 8 9 undefined
+// when there is less values and more variable, the variable which dont have value store undefine 
+//! Now can also provide defalut values to the variable 
+const [m = 1, n = 1, o = 1] = [6, 7];
+console.log(m, n, o); // 6 7 1
+
+
+//*=========================Destructuring of objects=========================
+//!syntax - we have to provide the variable name which exactly match the property name of the objects.
+
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+// Classico Italiano
+// {
+//   thu: { open: 12, close: 22 },
+//   fri: { open: 11, close: 23 },
+//   sat: { open: 0, close: 24 }
+// }
+// [ 'Italian', 'Pizzeria', 'Vegetarian', 'Organic' ]
+
+//? Variable name different from the property name 
+const {name: dish, openingHours: hours, categories: types} = restaurant;
+console.log(dish, hours, types);
+
+// Classico Italiano
+// {
+//   thu: { open: 12, close: 22 },
+//   fri: { open: 11, close: 23 },
+//   sat: { open: 0, close: 24 }
+// } 
+// [ 'Italian', 'Pizzeria', 'Vegetarian', 'Organic' ]
+
+//? Default value 
+//!menu is not a key in the object restaurant so it will remain empty but the staterMenu will get filled
+const {menu = [], starterMenu:staters = []} = restaurant;
+console.log(menu, staters); // [] [ 'Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad' ]
+
+//? Mutating Variable
+//! here we have two variable one and two we want to rewrite the variable in destructuring form then
+let one = 23;
+let two = 31;
+const obj = {one: 21, two:24, three:31};
+({one, two} = obj); //! we have to wrap this up in the paranthesis to make sure there is no error 
+console.log(one, two); // 21 24
+
+
+//? Nested object destructuring
