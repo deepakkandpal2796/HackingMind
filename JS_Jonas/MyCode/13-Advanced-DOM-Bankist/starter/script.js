1b7486bb97e24004366c45ed1a9588d8b71a6018
Deepak <deepakkandpal2796@gmail.com>
2024-04-24 07:39:58 +0530

    new codw

--- JS_Jonas/MyCode/13-Advanced-DOM-Bankist/starter/script.js
+++ JS_Jonas/MyCode/13-Advanced-DOM-Bankist/starter/script.js
@@ -3,12 +3,15 @@
 ///////////////////////////////////////
 // Modal window
 
+//Selected Elements 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
-const openModal = function () {
+const header = document.querySelector('header');
+//Functions
+const openModal = function (e) {
+  e.preventDefault(); //! on click of the hyperlink the page refresh
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +21,11 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+
+//Event listners 
+btnsOpenModal.forEach(element => {
+  element.addEventListener('click', openModal);
+});
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +35,42 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//Creating and inserting element 
+
+//--------create element and add class------------
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent -> we use this to fill the text content only
+message.innerHTML = "We use cookies for improved functionality and analytics <button class = 'btn btn-close-cookie'> Got it </button>";
+
+
+//-------------Append and prepend-----------------
+// header.prepend(message)
+header.append(message);
+// you can use one element at a time only if you append and prepend the element then, the method which runds later runs.
+//* if we want to apply multiple coppy of the same element then
+//header.append(message.cloneNode(true)); //we have made a copy and appended it 
+
+//-------------Before and after--------------------
+// header.before(message)
+//header.after(message)
+
+//--------------Remove the element-------------------
+document.querySelector('.btn-close-cookie').addEventListener('click', function(){
+  message.remove();
+})
+
+//--------------Adding Styles----------------------
+
+//* this will add the inline property to the html element
+message.style.background = '#37383d';
+message.style.width = '120%'
+
+console.log(message.style.width)
+console.log(message.style.color)//Error : as the color property is not inline defined
+
+//* to get the css property of the object which are not inlin
+console.log(getComputedStyle(message).height); 
+
+message.style.height = parseFloat(getComputedStyle(message).height)+30+ "px";
